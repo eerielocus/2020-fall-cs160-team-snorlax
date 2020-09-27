01e91d930c327c54b51511f4eab13ad32fa38e97
jakerosen <jakesrosen@gmail.com>
2020-09-27 11:36:29 -0700

    Added lombok @data feature and commented out the redundant code

--- prototype/src/main/java/com/snorlax/model/Employee.java
+++ prototype/src/main/java/com/snorlax/model/Employee.java
@@ -1,12 +1,14 @@
 package com.snorlax.model;
 
-import java.util.Objects;
+import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity
+@Data // automatically generate toString, hashCode, equals, getters, and
+      // setters
+@Entity // generate a table out of this object
 public class Employee {
 
   private @Id @GeneratedValue Long id;
@@ -22,62 +24,62 @@ public class Employee {
     this.description = description;
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Employee employee = (Employee) o;
-    return Objects.equals(id, employee.id) &&
-      Objects.equals(firstName, employee.firstName) &&
-      Objects.equals(lastName, employee.lastName) &&
-      Objects.equals(description, employee.description);
-  }
+  // @Override
+  // public boolean equals(Object o) {
+  //   if (this == o) return true;
+  //   if (o == null || getClass() != o.getClass()) return false;
+  //   Employee employee = (Employee) o;
+  //   return Objects.equals(id, employee.id) &&
+  //     Objects.equals(firstName, employee.firstName) &&
+  //     Objects.equals(lastName, employee.lastName) &&
+  //     Objects.equals(description, employee.description);
+  // }
 
-  @Override
-  public int hashCode() {
+  // @Override
+  // public int hashCode() {
 
-    return Objects.hash(id, firstName, lastName, description);
-  }
+  //   return Objects.hash(id, firstName, lastName, description);
+  // }
 
-  public Long getId() {
-    return id;
-  }
+  // public Long getId() {
+  //   return id;
+  // }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+  // public void setId(Long id) {
+  //   this.id = id;
+  // }
 
-  public String getFirstName() {
-    return firstName;
-  }
+  // public String getFirstName() {
+  //   return firstName;
+  // }
 
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
+  // public void setFirstName(String firstName) {
+  //   this.firstName = firstName;
+  // }
 
-  public String getLastName() {
-    return lastName;
-  }
+  // public String getLastName() {
+  //   return lastName;
+  // }
 
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
+  // public void setLastName(String lastName) {
+  //   this.lastName = lastName;
+  // }
 
-  public String getDescription() {
-    return description;
-  }
+  // public String getDescription() {
+  //   return description;
+  // }
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+  // public void setDescription(String description) {
+  //   this.description = description;
+  // }
 
-  @Override
-  public String toString() {
-    return "Employee{" +
-      "id=" + id +
-      ", firstName='" + firstName + '\'' +
-      ", lastName='" + lastName + '\'' +
-      ", description='" + description + '\'' +
-      '}';
-  }
+  // @Override
+  // public String toString() {
+  //   return "Employee{" +
+  //     "id=" + id +
+  //     ", firstName='" + firstName + '\'' +
+  //     ", lastName='" + lastName + '\'' +
+  //     ", description='" + description + '\'' +
+  //     '}';
+  // }
 }
