be6f7333cf8851e01fdad19934214ba1659ccbc5
jakerosen <jakesrosen@gmail.com>
2020-11-16 00:18:14 -0800

    testing image upload doesn't work yet

--- project/src/test/java/com/snorlax/SnorlaxApplicationTests.java
+++ project/src/test/java/com/snorlax/SnorlaxApplicationTests.java
@@ -3,11 +3,42 @@ package com.snorlax;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.*;
+
+import
+  org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.web.multipart.MultipartFile;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static
+  org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 class SnorlaxApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  private MockMvc mvc;
+
+  @Test
+  void contextLoads() {
+  }
+
+  @Test
+  void addImageTest() throws Exception {
+    byte[] data = Files.readAllBytes(Paths.get("imageTests/cat.jpg"));
+    MockMultipartFile file = new MockMultipartFile("cat.jpg", "cat.jpg",
+        "image/jpeg", data);
+    mvc.perform(MockMvcRequestBuilders.multipart("/api/images")
+      .file(file)
+      .param("ip", "555"))
+      .andExpect(status().is(201))
+      ;
+  }
 
 }
