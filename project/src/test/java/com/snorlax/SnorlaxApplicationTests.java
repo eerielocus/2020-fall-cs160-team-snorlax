5fd2412d3a42981502c16f6890b447c7c3e9bdd6
jakerosen <jakesrosen@gmail.com>
2020-11-16 14:29:37 -0800

    selenium project integration

--- project/src/test/java/com/snorlax/SnorlaxApplicationTests.java
+++ project/src/test/java/com/snorlax/SnorlaxApplicationTests.java
@@ -3,17 +3,22 @@ package com.snorlax;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
 import static org.junit.Assert.*;
 
 import
-  org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 // import org.springframework.web.multipart.MultipartFile;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static
-  org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -39,6 +44,31 @@ class SnorlaxApplicationTests {
       .param("ip", "555"))
       .andExpect(status().is(201))
       ;
+  }
+
+  @Test
+  public void dropzoneTest() Throws Exception {
+    // Create a new instance of the html unit driver
+    // Notice that the remainder of the code relies on the interface,
+    // not the implementation.
+    WebDriver driver = new HtmlUnitDriver();
+
+    // And now use this to visit Google
+    driver.get("http://reactdropzone.azurewebsites.net/example/");
+
+    // Find the text input element by its name
+    WebElement element = driver.findElement(By.cssSelector("input[type='file']"));
+
+    // Enter something to search for
+    element.sendKeys("image.png");
+
+    // Now submit the form. WebDriver will find the form for us from the element
+    element.submit();
+
+    // Check the title of the page
+    System.out.println("Page title is: " + driver.getTitle());
+
+    driver.quit();
   }
 
 }
